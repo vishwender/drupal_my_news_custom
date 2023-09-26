c1768386e7ff70c8801cfdfb6978b68dd01dac7f
vishu <vishu@vishus-MacBook-Air.local>
2023-09-26 16:53:17 +0530

    new route and functions added

--- modules/custom/mynews/src/Controller/MyNewsController.php
+++ modules/custom/mynews/src/Controller/MyNewsController.php
@@ -3,14 +3,30 @@ namespace Drupal\mynews\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\Component\Serialization\Json;
+use Symfony\Component\HttpFoundation\Request;
 
 class MyNewsController extends ControllerBase{
-    
-    public function getInNews(){
+
+    public function getConfig(){
         $mynews_config = \Drupal::config('mynews.settings');
         $mynews_access_token = $mynews_config->get('mynews_access_token');
+        return $mynews_access_token;
+    }
+
+    public function getNewsByCategory($category){
+        $config  = $this->getConfig();
         $client  = \Drupal::httpClient();
-        $url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=$mynews_access_token";
+        $url = "https://newsapi.org/v2/top-headlines?category=$category&language=en&apiKey=$config";
+        $request = $client->get(trim($url));  
+        $response = $request->getBody()->getContents();
+        $result = json::decode($response);
+        return $result;
+    }
+    
+    public function getInNews(){
+        $config  = $this->getConfig();
+        $client  = \Drupal::httpClient();
+        $url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=$config";
         $request = $client->get(trim($url));  
         $response = $request->getBody()->getContents();
         $result = json::decode($response);
@@ -22,10 +38,9 @@ class MyNewsController extends ControllerBase{
     }
 
     public function getUsNews(){
-        $mynews_config = \Drupal::config('mynews.settings');
-        $mynews_access_token = $mynews_config->get('mynews_access_token');
+        $config = $this->getConfig();
         $client  = \Drupal::httpClient();
-        $url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=$mynews_access_token";
+        $url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=$config";
         $request = $client->get(trim($url));  
         $response = $request->getBody()->getContents();
         $result = json::decode($response);
@@ -34,5 +49,24 @@ class MyNewsController extends ControllerBase{
                 '#theme'=>'mynews_template',
                 '#data' => $result['articles']
                ];
+    }
+
+    public function getSources(){
+        $config =  $this->getConfig();
+        $client = \Drupal::httpClient();
+        $url = "https://newsapi.org/v2/top-headlines/sources?apiKey=$config";
+        $request = $client->get(trim($url));
+        $response = $request->getBody()->getContents();
+        $result = json::decode($response);
+        echo '<pre>'; print_r($result['sources']);die();
+    }
+
+    public function getCategoryNews(Request $request, $url){
+        $result = $this->getNewsByCategory($url);
+        //echo '<pre>'; print_r($result);die();
+        return [
+            '#theme'=>'mynews_template',
+            '#data' => $result['articles']
+           ];
     }
 }
